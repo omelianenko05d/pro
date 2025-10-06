00e18cdcb4b8968ff16077f42166fcd3e3dc5f54
omelianenko05d <OmeDar23@student.skolahostivar.cz>
2025-10-06 12:17:56 +0200

    Add files via upload

---
+++ C005.js
@@ -0,0 +1,5 @@
+let jmeno = prompt("Jak se jmenuješ?");
+let vek = prompt("Kolik Vam je let?");
+let email = prompt("Jaky je Vas email?");
+
+console.log("Dobry den" + jmeno , "Vas uvedeny email je" + email + "a je Vam" + Number(vek) + "let");
