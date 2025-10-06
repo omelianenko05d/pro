bf6975f4e14a5952cb412dd570a26b6b6bb8aaa9
omelianenko05d <OmeDar23@student.skolahostivar.cz>
2025-10-06 12:04:33 +0200

    Add files via upload

---
+++ P006-prompt.js
@@ -0,0 +1,11 @@
+//uzivatelsky vstup z konzole
+let promenna = prompt("Ukazka prompt.", "Zkus neco napsat:");
+// 1.text je nad vstupem, 2.text je predbolba pokud neco nezada uzivatel
+console.log(promenna);
+
+let jmeno = prompt("Zadej jmeno:");
+console.log(jmeno);
+
+//prevod na spravny datovy typ
+let cislo = prompt("Zadej cislo: ");
+console.log(Number(cislo) + 5);
