450863f308af8a238ab8b43d8146df6cee08ac0c
omelianenko05d <OmeDar23@student.skolahostivar.cz>
2025-10-20 12:18:21 +0200

    Add files via upload

---
+++ C013.js
@@ -0,0 +1,9 @@
+//js - nejdriv nechte mimo funkce zadat uzivatele jeho vek
+// nasledne privolejte funkce, ktera jeho vek doplni do vety - "me jemno je bruno, jsem z rakouska a je mi " + xxx + "let"
+
+let vek = prompt("Kolik Vam je let?");
+
+function neco(a){
+    console.log("me jmeno je bruno, jsem z rakouska" + a +"let");
+}
+neco(vek);
